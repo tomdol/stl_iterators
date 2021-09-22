755abeff93613b799778aa73ad9fe0a929fbf7cf
tomdol <tomasz.dolbniak@protonmail.com>
2021-09-22 21:14:45 +0200

    Code formatting with config

--- code/src/tensor.hpp
+++ code/src/tensor.hpp
@@ -3,7 +3,8 @@
 #include "shape.hpp"
 #include <memory>
 
-template <typename T> struct Tensor {
+template <typename T>
+struct Tensor {
     Tensor(Shape&& shape, std::initializer_list<T> values)
         : _shape{std::move(shape)}, _buffer{std::make_unique<T[]>(_shape.shape_capacity())} {
         std::copy(std::begin(values), std::end(values), _buffer.get());
@@ -12,11 +13,9 @@ template <typename T> struct Tensor {
     const T* begin() const { return _buffer.get(); }
     const T* end() const { return _buffer.get() + _shape.shape_capacity(); }
 
-    const Shape& shape() const noexcept {
-        return _shape;
-    }
+    const Shape& shape() const noexcept { return _shape; }
 
-private:
+  private:
     const Shape _shape;
     std::unique_ptr<T[]> _buffer;
 };

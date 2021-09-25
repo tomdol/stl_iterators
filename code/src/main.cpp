046be423a96f8ca682af40d729a0ad99ce5e180d
tomdol <tomasz.dolbniak@protonmail.com>
2021-09-25 18:14:07 +0200

    Preparation for more max_pool versions

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,6 +3,11 @@
 #include "max_pool.hpp"
 #include "tensor.hpp"
 
+void infer_max_pool(const Tensor<int32_t>& data, Tensor<int32_t>& output, const Shape& kernel, const Shape& pads_begin,
+                    const Shape& pads_end) {
+    max_pool(data, output, kernel, pads_begin, pads_end);
+}
+
 int main(int argc, char** argv) {
     // clang-format off
     const auto data = Tensor<int32_t>{Shape{1, 1, 4, 4}, 
@@ -11,13 +16,15 @@ int main(int argc, char** argv) {
          17,  4, 22, 28, 
         -20, 11,  0, 17}};
     // clang-format on
-    
+
     auto output = Tensor<int32_t>{Shape{1, 1, 3, 3}};
+    const auto kernel = Shape{2, 2};
+    const auto pads_begin = Shape{0, 0};
+    const auto pads_end = Shape{0, 0};
 
     std::cout << data << std::endl;
 
-    max_pool(data, output, Shape{2, 2}, Shape{0, 0}, Shape{0, 0});
-
+    infer_max_pool(data, output, kernel, pads_begin, pads_end);
     std::cout << output << std::endl;
 
     return 0;
